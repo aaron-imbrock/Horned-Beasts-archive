cec12d0cda23f9e49e5652b5768a90d437c6d8c3
Aaron Imbrock <aaron@sizza.net>
2023-04-10 21:32:58 -0600

    Trying different ways to get imgs to work

--- src/main.js
+++ src/main.js
@@ -7,16 +7,6 @@ const imgs = [
     'https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp'
 ]
 
-// The Main component needs to pass title, imageUrl, and description into 
-// each HornedBeast component. 
-
-// For the purpose of this lab, you can pass whatever title, url and description 
-// that you want into each HornedBeast component.
-
-// - The HornedBeast component needs to contain 
-// an `<h2>` that displays the title of the animal, 
-// an `<img>` element with src, alt and title attributes, 
-// and a `<p>` that displays the description.
 
 class Main extends React.Component {
     render() {
