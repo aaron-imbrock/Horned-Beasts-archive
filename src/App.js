e5a110f54e25fca5febdc9ca4475ab477f8d98b6
Aaron Imbrock <aaron@sizza.net>
2023-04-12 21:36:47 -0600

    feature requirements met

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import imageUrls from './data.json';
 
 function App() {
   return (
    <>
     <Header title='Horned Beasts'/>
-    <Main />
+    <Main heading="Main soon" imageUrls={imageUrls}/>
     <Footer title='Your opinion counts!'/>
    </>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,48 @@
 import { Component } from 'react';
+import Image from 'react-bootstrap/Image'
 
 class Main extends Component {
     render() {
       return (
-        <header>
-          <h1>Main Soon</h1>
-        </header>
+        <div>
+          <h2>{this.props.heading}</h2>
+          {
+            this.props.imageUrls.map(imageInfo => <HornedBeast info={imageInfo} />)
+          }
+        </div>
       );
     }
   }
 
+class HornedBeast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "Nay",
+    }
+  }
+
+  // NOTE: use arrow functions for event handlers to retain proper contextual "this"
+  handleClick = () => {
+    // want to flip the yay to nay or vice versa
+    // in other words update state
+    // It's a rule - you set state using the following method:
+    const newStatus = this.state.status === "Nay" ? "Yay" : "Nay";
+
+    this.setState({
+      status: newStatus
+    });
+
+  }
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        <p>{this.props.info.title}</p>
+        <Image src={this.props.info.image_url} alt='some horned beast' rounded fluid />
+        <p>{this.props.info.description}</p>
+        <p>{this.state.status}</p>
+      </div>
+    );
+  }
+}
   export default Main;
